2172edfd39f294dd19d58cb6a7770e176591e660
Tibi Kolozsi <tibi.kolozsi@gmail.com>
2014-11-02 15:07:39 +0100

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -9,16 +9,16 @@
     character = (Character *)[CCBReader load:@"Character"];
     [physicsNode addChild:character];
     
-    [self addObstacle];
-    timeSinceObstacle = 0.0f;
+//    [self addObstacle];
+//    timeSinceObstacle = 0.0f;
 }
 
 - (void)update:(CCTime)delta {
-    timeSinceObstacle += delta;
-    if (timeSinceObstacle > 2.0f) {
-        [self addObstacle];
-        timeSinceObstacle = 0.0f;
-    }
+//    timeSinceObstacle += delta;
+//    if (timeSinceObstacle > 2.0f) {
+//        [self addObstacle];
+//        timeSinceObstacle = 0.0f;
+//    }
 }
 
 // put new methods here
